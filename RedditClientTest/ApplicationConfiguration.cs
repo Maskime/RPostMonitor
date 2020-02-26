5125020934c95894901b7a3a243cae64d9761cc2
Maxime Faye (Serial SA) <Maxime.Faye1@ge.com>
2020-02-26 16:15:33 +0100

    Initial commit

---
+++ RedditClientTest/ApplicationConfiguration.cs
@@ -0,0 +1,18 @@
+// ************************************************************************************************
+// 
+//  © 2019       General Electric Company
+// 
+//  Description  See class summary below.
+// 
+//  History      See source code control system.
+// 
+// ************************************************************************************************
+
+namespace RedditClientTest
+{
+    public class ApplicationConfiguration
+    {
+        public string Name { get; set; }
+        public RedditConfiguration Reddit { get; set; }
+    }
+}
---
+++ RedditClientTest/RedditConfiguration.cs
@@ -0,0 +1,21 @@
+// ************************************************************************************************
+// 
+//  © 2019       General Electric Company
+// 
+//  Description  See class summary below.
+// 
+//  History      See source code control system.
+// 
+// ************************************************************************************************
+
+namespace RedditClientTest
+{
+    public class RedditConfiguration
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string RedirectURI { get; set; }
+
+        public string Username { get; set; }
+    }
+}
