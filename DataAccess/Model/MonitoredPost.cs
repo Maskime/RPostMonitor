4066a561b0aff74d2cd3d48dff3cc4b699e22950
Maxime Faye (Serial SA) <Maxime.Faye1@ge.com>
2020-06-05 15:52:36 +0200

    forgot data access project

---
+++ DataAccess/Model/MonitoredPost.cs
@@ -0,0 +1,17 @@
+﻿// ************************************************************************************************
+// 
+//  © 2019       General Electric Company
+// 
+//  Description  See class summary below.
+// 
+//  History      See source code control system.
+// 
+// ************************************************************************************************
+
+namespace DataAccess.Model
+{
+    public class MonitoredPost
+    {
+        
+    }
+}
