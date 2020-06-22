5fbe7f113462ecf3ea490593c0db1b7cb967e849
Maxime Faye (Serial SA) <Maxime.Faye1@ge.com>
2020-06-22 11:56:26 +0200

    Making the project a bit more general

--- PostMonitor/Config/PollerConfiguration.cs
+++ PostMonitor/Config/PollerConfiguration.cs
@@ -4,8 +4,8 @@ namespace PostMonitor.Config
     {
         public const string ConfigKey = "Poller";
         
-        public string Name { get; set; }
-
         public string DownloadDir { get; set; }
+
+        public string  SubToWatch { get; set; }
     }
 }
