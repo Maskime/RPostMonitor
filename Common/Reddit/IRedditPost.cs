38bf77a97f9be0c39b8252684b732c9136e1154c
Maxime Faye (Serial SA) <Maxime.Faye1@ge.com>
2020-06-24 18:29:36 +0200

    WIP historization of posts

--- Common/Reddit/IRedditPost.cs
+++ Common/Reddit/IRedditPost.cs
@@ -4,13 +4,45 @@ namespace Common.Reddit
 {
     public interface IRedditPost
     {
-        string Author { get; set; }
-        DateTimeOffset CreatedAt { get; set; }
+        DateTimeOffset Created { get; set; }
+        DateTimeOffset CreatedUTC { get; set; }
         DateTimeOffset FetchedAt { get; set; }
-        string RedditId { get; set; }
-        string Permalink { get; set; }
-        string Url { get; set; }
 
+        TimeSpan TimeSinceFetch { get; set; }
+
+        Nullable<int> NumReports { get; set; }
+        Nullable<int> ReportCount { get; set; }
+
+        string ApprovedBy { get; set; }
+        string AuthorFlairCssClass { get; set; }
+        string AuthorFlairText { get; set; }
+        string AuthorName { get; set; }
+        string BannedBy { get; set; }
+        int CommentCount { get; set; }
+        string Domain { get; set; }
+        int Downvotes { get; set; }
+        bool Edited { get; set; }
+        string FullName { get; set; }
+        int Gilded { get; set; }
+        string Id { get; set; }
+        bool IsArchived { get; set; }
+        bool IsSelfPost { get; set; }
+        bool IsSpoiler { get; set; }
+        bool IsStickied { get; set; }
+        string Kind { get; set; }
+        string LinkFlairCssClass { get; set; }
+        string LinkFlairText { get; set; }
+        bool NSFW { get; set; }
+        Uri Permalink { get; set; }
+        bool Saved { get; set; }
+        int Score { get; set; }
+        string SelfText { get; set; }
+        string SelfTextHtml { get; set; }
+        string Shortlink { get; set; }
+        string SubredditName { get; set; }
+        Uri Thumbnail { get; set; }
         string Title { get; set; }
+        int Upvotes { get; set; }
+        Uri Url { get; set; }
     }
 }
